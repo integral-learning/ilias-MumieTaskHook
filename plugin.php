76fc9bf89d34ea0bb30cd5b38c7b470e565f8b5e
tobias <tobias.goltz@integral-learning.de>
2023-06-14 14:08:44 +0200

    Set compatability with ilias version to 7.22

--- plugin.php
+++ plugin.php
@@ -12,7 +12,7 @@ $version = "2.0.0";
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "5.4.4";
-$ilias_max_version = "7.15";
+$ilias_max_version = "7.22";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Tobias Goltz";
