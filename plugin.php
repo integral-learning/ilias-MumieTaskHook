5e262987fcc834eb357f0e743c9b963f1361dbd7
Dung Pham <dung.pham@integral-learning.de>
2026-05-27 11:57:07 +0200

    remove removed logout script

--- plugin.php
+++ plugin.php
@@ -7,11 +7,11 @@
 $id = 'mumie_task_events';
 
 // code version; must be changed for all code changes
-$version = '4.0.1';
+$version = '4.0.2';
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = '9.0';
+$ilias_min_version = '10.0';
 $ilias_max_version = '10.99';
 
 // optional, but useful: Add one or more responsible persons and a contact email
